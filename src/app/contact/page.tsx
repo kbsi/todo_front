2dcb270f8434041a3c2114ed90d69342cc597349
Kevin Boulais <>
2025-05-14 12:10:47 +0200

    refactor: restructure Contact component to include Navbar and improve layout

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Contact() {
     return (
-        <div className="flex justify-center items-center h-screen">
-        <h1 className="text-5xl font-bold">Contact 📫</h1>
+        <div className="relative w-full flex items-center justify-center">
+            <Navbar className="top-2" />
+            <div className="flex justify-center items-center h-screen">
+                <h1 className="text-5xl font-bold">Contact 📫</h1>
+            </div>
         </div>
-        );
-    }
+    );
+}
+
+function Navbar({ className }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div
+            className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <MenuItem setActive={setActive} active={active} item="Menu">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/">Todo list</HoveredLink>
+                        <HoveredLink href="/contact">Contact</HoveredLink>
+                    </div>
+                </MenuItem>
+            </Menu>
+        </div>
+    );
+}
